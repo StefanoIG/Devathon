81930ddf516ff3ea209e3ed692443affdca7ae52
StefanoIG <stefano20033@gmail.com>
2024-08-05 15:53:37 -0500

    feat: Configure email backend for SMTP
    Update `devathon/settings.py` to configure the email backend for SMTP. Set the `EMAIL_BACKEND` to `'django.core.mail.backends.smtp.EmailBackend'` and specify the SMTP server details such as `EMAIL_HOST`, `EMAIL_PORT`, `EMAIL_USE_TLS`, `EMAIL_HOST_USER`, and `EMAIL_HOST_PASSWORD`.

--- devathon/settings.py
+++ devathon/settings.py
@@ -160,3 +160,10 @@ AUTH_USER_MODEL = 'usuarios.Cliente'  # Asegúrate de que esto esté configurado
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'stefano20033@gmail.com'
+EMAIL_HOST_PASSWORD = 'fynl zjhq mycd qlly'
