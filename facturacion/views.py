20e20fbee67e29b7321c22cf7e31eb58996df2cd
StefanoIG <stefano20033@gmail.com>
2024-08-17 14:19:14 -0500

    feat: Add lazy loading for images in facturacion/views.py
    This commit adds lazy loading for images in the `facturacion/views.py` file. It imports the necessary modules and adds the `Image` class to the `reportlab.platypus` import statement. It also adds the `Image` class to the `elements` list for generating the PDF.

--- facturacion/views.py
+++ facturacion/views.py
@@ -14,8 +14,10 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib import colors
 from reportlab.lib.units import inch
 from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from decimal import Decimal
+import os
 
 
 # Permiso personalizado para permitir solo a empleados y admins
@@ -80,51 +82,115 @@ class FacturaDownloadView(APIView):
             return Response({"error": "Factura no encontrada"}, status=status.HTTP_404_NOT_FOUND)
 
     def generate_pdf(self, factura):
-        # Mismo método de generación de PDF que en FacturaCreateView
         buffer = BytesIO()
         doc = SimpleDocTemplate(buffer, pagesize=letter)
         elements = []
-        # Estilo de la tabla
-        style = getSampleStyleSheet()
-        normal_style = style["Normal"]
 
-        # Calcular los valores
+        # Datos de la factura
         monto_total = factura.monto_total
         iva_porcentaje = Decimal('0.15')
         precio_sin_iva = monto_total / (1 + iva_porcentaje)
         iva = monto_total * iva_porcentaje
 
-        # Datos para la tabla
-        data = [
-            ["Factura No:", f"{factura.id}"],
-            ["Cliente:", f"{factura.cliente.nombre}"],
-            ["Fecha:", f"{factura.fecha_vencimiento.strftime('%d/%m/%Y')}"],
-            ["Precio sin IVA:", f"${precio_sin_iva:.2f}"],
-            ["Porentaje de iva:", f"15%"],
-            ["IVA (15%):", f"${iva:.2f}"],
-            ["Precio final:", f"${monto_total:.2f}"],
+        styles = getSampleStyleSheet()
+        header_style = ParagraphStyle(name='Header', fontSize=16, spaceAfter=12, alignment=0)  # Alineación izquierda
+        title_style = ParagraphStyle(name='Title', fontSize=12, spaceAfter=8, alignment=0)  # Tamaño de fuente menor
+        normal_style = styles["Normal"]
+
+        # Agregar logo de la entidad emisora
+        logo_path = os.path.join(os.path.dirname(__file__), 'img', 'logo.png')
+        try:
+            logo = Image(logo_path)
+            logo.drawHeight = 1.5 * inch
+            logo.drawWidth = 1.5 * inch
+            logo_table = Table([[logo]], colWidths=[1.5 * inch])
+            logo_table.setStyle(TableStyle([
+                ('ALIGN', (0, 0), (0, 0), 'RIGHT'),
+                ('VALIGN', (0, 0), (0, 0), 'TOP')
+            ]))
+            elements.append(logo_table)
+        except:
+            pass
+
+        # Encabezado de la factura
+        elements.append(Paragraph("Factura", header_style))
+        elements.append(Spacer(1, 12))
+
+        # Información de la factura
+        data_info = [
+            ["De:", "Changarro de Mexacol.", "", "N° de Factura", f"{factura.id}"],
+            ["", "Algun lugar del mundo", "", "Fecha de emisión", f"{factura.fecha_vencimiento.strftime('%d/%m/%Y')}"],
+            ["", "Hecho con amor <3", "", "", ""],
+            ["Facturar a:", f"{factura.cliente.nombre} {factura.cliente.apellido}", "", "Fecha de pago", f"{factura.fecha_vencimiento.strftime('%d/%m/%Y')}"]
         ]
-
-        # Crear la tabla
-        table = Table(data, colWidths=[2.5 * inch, 4 * inch])
-        table.setStyle(TableStyle([
-            ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-            ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        table_info = Table(data_info, colWidths=[1 * inch, 2.5 * inch, 0.5 * inch, 2 * inch, 2 * inch])
+        table_info.setStyle(TableStyle([
             ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
-            ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-            ('FONTSIZE', (0, 0), (-1, 0), 12),
-            ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-            ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
-            ('GRID', (0, 0), (-1, -1), 1, colors.black),
+            ('FONTNAME', (0, 0), (-1, -1), 'Helvetica'),
+            ('FONTSIZE', (0, 0), (-1, -1), 12),
+            ('SPAN', (1, 0), (1, 1)),  # Combina celdas para la dirección
+            ('SPAN', (1, 3), (1, 3)),  # Combina celdas para la dirección del cliente
+            ('SPAN', (0, 3), (0, 3)),  # Combina celdas para "Facturar a:"
         ]))
+        elements.append(table_info)
+        elements.append(Spacer(1, 12))
 
-        # Añadir la tabla a los elementos del documento
-        elements.append(table)
+        # Detalles de la factura
+        data_detalle = [
+            ["CANT.", "DESCRIPCIÓN", "PRECIO UNITARIO", "IMPORTE"],
+            ["1", "Consumo de alimentos", f"${monto_total:.2f}", f"${iva:.2f}"],
+            ["", "", "", ""],
+            ["", "", "", ""],
+            ["", "", "Subtotal", f"${precio_sin_iva:.2f}"],
+            ["", "", "IVA 15.0%", f"${iva:.2f}"],
+            ["", "", "TOTAL", f"${monto_total:.2f}"]
+        ]
+        table_detalle = Table(data_detalle, colWidths=[1 * inch, 3 * inch, 2 * inch, 2 * inch])
+        table_detalle.setStyle(TableStyle([
+            ('GRID', (0, 0), (-1, -1), 0.5, colors.black),
+            ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+            ('ALIGN', (2, 0), (-1, 0), 'RIGHT'),  # Alinear encabezados a la derecha
+            ('ALIGN', (2, 1), (-1, -1), 'RIGHT'),  # Alinear precios e importes a la derecha
+            ('FONTSIZE', (0, 0), (-1, 0), 12),
+            ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ]))
+        elements.append(table_detalle)
+        elements.append(Spacer(1, 24))
+
+        # Tabla de total
+        data_total = [["Total", f"${monto_total:.2f}"]]
+        table_total = Table(data_total, colWidths=[3.5 * inch, 2 * inch])
+        table_total.setStyle(TableStyle([
+            ('GRID', (0, 0), (-1, -1), 1, colors.black),
+            ('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey),
+            ('ALIGN', (1, 0), (1, 0), 'RIGHT'),
+            ('FONTSIZE', (0, 0), (1, 0), 14),
+            ('FONTNAME', (0, 0), (1, 0), 'Helvetica-Bold'),
+            ('BOTTOMPADDING', (0, 0), (-1, -1), 10),
+            ('TOPPADDING', (0, 0), (-1, -1), 10),
+        ]))
+        elements.append(table_total)
+        elements.append(Spacer(1, 24))
+
+        # Condiciones y forma de pago
+        elements.append(Paragraph("Condiciones y forma de pago", title_style))
+        elements.append(Spacer(1, 1))
+        elements.append(Paragraph("Pago en efectivo, tarjeta de crédito o transferencia bancaria.", normal_style))
+        elements.append(Paragraph("Pago realizado en el local.", normal_style))
+        elements.append(Paragraph("0 días plazo de pago.", normal_style))
+        elements.append(Spacer(1, 12))
+
+        # Nota y firma
+        elements.append(Paragraph("Gracias por su preferencia. Si tiene alguna pregunta sobre esta factura, no dude en contactarnos.", normal_style))
+        elements.append(Spacer(1, 12))
+        elements.append(Paragraph("Atentamente, el equipo de Changarro de Mexacol.", normal_style))
+        elements.append(Spacer(1, 12))
 
         # Generar el PDF
         doc.build(elements)
         buffer.seek(0)
         return buffer
+
 
 
 
